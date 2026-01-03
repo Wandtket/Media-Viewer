4a0322c5946c85588abee978f97e5911ac137be9
Tyler Wandtke <wandtket@gmail.com>
2026-01-03 23:03:24 +1300

    Added Premiere pro templates to dependencies.

---
+++ Media-Viewer/Dependencies/Template.jsx
@@ -0,0 +1,20 @@
+﻿app.openDocument("{PRPROJ}");
+
+var mediaFile = new File("{FILEPATH}");
+app.project.importFiles([mediaFile.fsName]);
+
+var root = app.project.rootItem;
+var clip = null;
+
+for (var i = 0; i < root.children.numItems; i++) {
+    var item = root.children[i];
+    if (item.name === mediaFile.name) {
+        clip = item;
+        break;
+    }
+}
+
+//For some reason creating timelines refuses to work properly
+//var newSeq = app.project.createNewSequenceFromClips(mediaFile.name + "_Sequence", [importedItem]);
+//newSeq.videoTracks[0].insertClip(clip, 0);
+//newSeq.audioTracks[0].insertClip(clip, 0);
