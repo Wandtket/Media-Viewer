8560cd9e9000d24451db413e75415064c5b1f2aa
Tyler Wandtke <wandtket@gmail.com>
2026-01-03 23:03:39 +1300

    Open action enum for opening files in different programs

---
+++ Media-Viewer/Enums/OpenAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaViewer.Enums
+{
+    public enum OpenAction
+    {
+        Editor,
+        Converter,
+        Upscaler
+    }
+}
